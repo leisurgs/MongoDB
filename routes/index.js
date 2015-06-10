de0bb1fd7705bb8c860953c2ef8f4092725a0834
George Leisure <[leisurgs@gmail.com]>
2015-06-10 15:20:30 -0700

    second commit

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var MongoClient = require('mongodb').MongoClient;
+var uuid = require('uuid');
+var myString = uuid.v4();
+
+MongoClient.connect('mongodb://127.0.0.1:27017/test', function(err, db) {
+  if (err) {
+    throw err;
+  }
+});
+  
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -7,3 +17,11 @@ router.get('/', function(req, res, next) {
 });
 
 module.exports = router;
+
+router.get('/', function(request, response) {
+  var url = request.body.EnterURL;
+  var alteredURL = client.set(url);
+  MongoClient.insert
+  response.render('index', {});
+  // index.jade needs a form to submit a URL for shortening
+});
